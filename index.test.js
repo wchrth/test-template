1fbffd4c1b3c49e0347524edaa31b5309e8fd7dc
Ratthanan <ratthanan@wongnai.com>
2019-10-08 10:46:23 +0700

    Add test

---
+++ index.test.js
@@ -0,0 +1,9 @@
+const f = arr => {};
+
+describe("sorting", () => {
+  it("(╯°□°）╯︵ ┻━┻", () => {
+    expect(f([1, 2, 4, 3])).toEqual(2);
+    expect(f([4, 2, 1, 3])).toEqual(0);
+    expect(f([1, 2, 3, 4])).toEqual(4);
+  });
+});
